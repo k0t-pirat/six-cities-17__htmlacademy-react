4f8b1bd5f2ff143935319cad80184b4435e5b023
Oleg <kot-pirat@bk.ru>
2024-12-18 16:16:28 +0300

    6.16. Контроль и ограничения (часть 2) #пункт 1

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -1,5 +1,6 @@
 import { OfferCard as TOfferCard } from '../../types/offer';
 import OfferCard from '../offer-card';
+import Sorting from '../sorting';
 
 type OffersListProps = {
   offerCards: TOfferCard[];
@@ -12,21 +13,7 @@ export default function OffersList({offerCards, setActiveOfferId, currentCity}: 
     <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
       <b className="places__found">{offerCards.length} places to stay in {currentCity}</b>
-      <form className="places__sorting" action="#" method="get">
-        <span className="places__sorting-caption">Sort by</span>
-        <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-          <svg className="places__sorting-arrow" width="7" height="4">
-            <use xlinkHref="#icon-arrow-select"></use>
-          </svg>
-        </span>
-        <ul className="places__options places__options--custom places__options--opened">
-          <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-          <li className="places__option" tabIndex={0}>Price: low to high</li>
-          <li className="places__option" tabIndex={0}>Price: high to low</li>
-          <li className="places__option" tabIndex={0}>Top rated first</li>
-        </ul>
-      </form>
+      <Sorting />
       <div className="cities__places-list places__list tabs__content">
         {offerCards.map((offerCard) => (
           <OfferCard key={offerCard.id} offerCard={offerCard} className="cities" setActiveOfferId={setActiveOfferId} />
